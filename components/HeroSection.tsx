616d90484c2d57c4c75f6fbd724e2618fa8fa216
manash9062162478 <manashbarai9062162478@gmail.com>
2024-07-15 21:22:15 +0530

    make-theme-provider-ssr

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -68,8 +68,11 @@ const HeroSection = () => {
             </ButtonsCard>
           </div>
           <div className="flex gap-4 mt-7 items-center z-50 ">
-            {mySocialLink.map((social:SocialLink)=>(
-              <ButtonsCard>
+            {mySocialLink.map((social:SocialLink,index:number)=>(
+              <div key={index}>
+
+              
+              <ButtonsCard >
               <button className={`relative transition-all duration-300 hover:scale-125  inline-flex h-12 overflow-hidden ${ social.id===1?'rounded-xl':'rounded-full'} p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50`}>
                 <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
                 <span className={`inline-flex h-full w-full cursor-pointer items-center justify-center ${ social.id===1?'rounded-xl':'rounded-full'} bg-slate-950 px-1 py-1 text-sm font-medium text-white backdrop-blur-3xl `}>
@@ -89,6 +92,7 @@ const HeroSection = () => {
                 </span>
               </button>
             </ButtonsCard>
+            </div>
             ))}
 
             
